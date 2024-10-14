01a917e3ca120e98503b07ee418e4433c9b6b4ea
Richard Essuman <ressuman001@gmail.com>
2024-10-14 09:30:42 +0000

    Refactor starting page component and update page titles

--- components/starting-page/starting-page.js
+++ components/starting-page/starting-page.js
@@ -1,6 +1,6 @@
 import classes from "./starting-page.module.css";
 
-export default function StartingPage() {
+export default function StartingPageContent() {
   // Show Link to Login page if NOT auth
 
   return (
