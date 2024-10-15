6722272bb5a3524aadd83b5ced9cb02d58f6f3c4
Richard Essuman <ressuman001@gmail.com>
2024-10-15 09:45:23 +0000

    "Updated package-lock.json, package.json, and pages/_app.js and pages/index.js files with new dependencies and layout changes."

---
+++ pages/api/auth/signUp.js
@@ -0,0 +1,50 @@
+import { hashPassword } from "@/helpers/hash";
+import { connectToDatabase } from "@/lib/db";
+
+export default async function handler(req, res) {
+  try {
+    if (req.method === "POST") {
+      const { email, password } = req.body;
+
+      if (
+        !email ||
+        email.trim() === "" ||
+        !email.includes("@") ||
+        !password ||
+        !password.trim().length < 7
+      ) {
+        res.status(422).json({ message: "Invalid input" });
+        return;
+      }
+
+      const { client, dbName } = await connectToDatabase("Auth");
+
+      const hashedPassword = hashPassword(password);
+
+      const newUser = {
+        email,
+        password: hashedPassword,
+        date: new Date(),
+      };
+
+      const insertResult = await insertDocument(
+        client,
+        dbName,
+        "users",
+        newUser
+      );
+
+      newUser._id = insertResult.insertedId;
+
+      res.status(201).json({
+        message: "Signed Up!",
+        data: newUser,
+      });
+    } else {
+      res.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Database connection failed" });
+  }
+}
