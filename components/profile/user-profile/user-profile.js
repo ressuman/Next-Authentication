e832cbc0e3149ef9679176725e41cc0f95ca9b7d
Richard Essuman <ressuman001@gmail.com>
2024-10-17 13:19:33 +0000

    "Comment out Next-Auth imports and related code in user-profile.js"

--- components/profile/user-profile/user-profile.js
+++ components/profile/user-profile/user-profile.js
@@ -1,33 +1,33 @@
 import ProfileForm from "../profile-form/profile-form";
 import classes from "./user-profile.module.css";
-import { useState, useEffect } from "react";
-import { useSession, getSession } from "next-auth/react";
+//import { useState, useEffect } from "react";
+//import { useSession, getSession } from "next-auth/react";
 
 export default function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
-  //const [loadedSession, setLoadedSession] = useState();
+  // const [isLoading, setIsLoading] = useState(true);
+  // //const [loadedSession, setLoadedSession] = useState();
 
-  useEffect(() => {
-    getSession().then((session) => {
-      //setLoadedSession(session);
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     //setLoadedSession(session);
 
-      if (!session) {
-        window.location.href = "/auth";
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
+  //     if (!session) {
+  //       window.location.href = "/auth";
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
 
-  //const { data: session, status } = useSession();
-  //const loading = status === "loading";
+  // //const { data: session, status } = useSession();
+  // //const loading = status === "loading";
 
-  // if (status === "loading") {
+  // // if (status === "loading") {
+  // //   return <p className={classes.profile}>Loading...</p>;
+  // // }
+  // if (isLoading) {
   //   return <p className={classes.profile}>Loading...</p>;
   // }
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
 
   return (
     <section className={classes.profile}>
